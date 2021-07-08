a14ea2d6e4006f80aee911a0423116782cf91ec3
Alfredo Cedeno <ajcborges@gmail.com>
2021-07-08 17:45:32 +1000

    Update main.tf

--- container/nginx-manager-proxy/main.tf
+++ container/nginx-manager-proxy/main.tf
@@ -7,18 +7,18 @@ resource "docker_container" "dckr-nmp-db" {
   restart               = "always"
   domainname            = var.container_domainname
   hostname              = var.container_hostname
-  env                   = [ 
-    "TZ=${var.TIME_ZONE}", 
-    "DB_MYSQL_HOST=var.db_mysql_host",
-    "DB_MYSQL_PORT=var.db_mysql_port",
-    "DB_MYSQL_USER=var.db_mysql_user",
-    "DB_MYSQL_PASSWORD=var.db_mysql_password",
-    "DB_MYSQL_NAME=var.db_mysql_name"
+  env = [ 
+    "TZ=${var.TIME_ZONE}",
+    "DB_MYSQL_HOST=${var.db_mysql[0]}", 
+    "DB_MYSQL_PORT=${var.db_mysql[1]}",
+    "DB_MYSQL_NAME=${var.db_mysql[2]}",
+    "DB_MYSQL_USER=${var.db_user}",
+    "DB_MYSQL_PASSWORD=${var.db_pass}"
     ]
 
   volumes {
   	volume_name    		  = "${docker_volume.nmp-db-vol.name}"
-  	container_path 		        = "/var/lib/mysql"
+  	container_path 		  = "/var/lib/mysql"
   }
   
   networks_advanced {
