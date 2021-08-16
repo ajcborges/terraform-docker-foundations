7ee1922171b45f38f72f043f00fedf40c0937127
Alfredo Cedeno <ajcborges@gmail.com>
2021-08-16 17:22:55 +1000

    Delete container/nginx-manager-proxy directory

--- container/nginx-manager-proxy/images.tf
+++
@@ -1,7 +0,0 @@
-# Docker Images
-#
-# Docker Image for nginx database Deployment
-resource "docker_image" "nmp-db-img" {
-   name                         = "jc21/mariadb-aria:latest"
-   keep_locally                 = true
-}
