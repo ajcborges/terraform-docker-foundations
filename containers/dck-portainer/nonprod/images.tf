f8802e047979164dd5716497e893a4e3a5d30137
Alfredo Cedeno <alfredo@cedeno.me>
2021-08-18 16:13:06 +1000

    adding Portainer into the Repo

---
+++ containers/dck-portainer/nonprod/images.tf
@@ -0,0 +1,7 @@
+# Docker Images
+#
+# Docker Image for Portainer Deployment
+resource "docker_image" "portainer-img" {
+   name             = "portainer/portainer-ce"
+   keep_locally     = true
+}
---
+++ containers/dck-portainer/nonprod/volumes.tf
@@ -0,0 +1,6 @@
+# Docker Volumes
+#
+# Docker Volume for Portainer Deployment
+resource "docker_volume" "portainer-vol" {
+   name = "vol-portainer-data"
+}
