f9e6d510d961b2237e671d0afc0a3bbae2557fd4
Alfredo Cedeno <alfredo@cedeno.me>
2021-08-18 16:25:28 +1000

    adding prod env for Portainer

---
+++ containers/dck-portainer/prod/variables.tf
@@ -0,0 +1,40 @@
+variable "ENV" {
+  type              = string
+  default           = "Production"
+}
+
+variable "container_port" {
+   description		= "port for Application"
+   default 		    = "9000"
+}
+
+variable "container_path" {
+   description		= "The path where data will be storage."
+   default 		    = "/media/docker/containers/portainer/config"
+}
+
+variable "container_hostname" {
+   description		= "Hostname of the container."
+   default 		    = "portainer"
+}
+
+variable "container_domainname" {
+   description		= "Domain name of the container.."
+   default 		    = "cedeno.me"
+}
+
+variable "traefik_dns_label" {
+   description      = "label for traefik."
+   default          = "portainer.cedeno.me"
+}
+
+variable "TIME_ZONE" {
+  type              = string
+  description       = "The timezone to be used (default: Australia/Melbourne)"
+  default           = "Australia/Melbourne"
+
+#  validation {
+#    condition      = length(var.TIME_ZONE) > 0
+#    error_message  = "Invalid TIME_ZONE specified."
+#  }
+}
