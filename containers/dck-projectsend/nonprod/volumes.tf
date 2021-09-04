67bc0903fa38ccf434fd55782376d3acf5ba6a6f
Alfredo Cedeno <alfredo@cedeno.me>
2021-09-04 22:22:57 +1000

    Fix Volume Name

--- containers/dck-projectsend/nonprod/volumes.tf
+++ containers/dck-projectsend/nonprod/volumes.tf
@@ -1,11 +1,19 @@
 # Docker Volumes
 #
 # Docker Volume for  Container
-resource "docker_volume" "projectsend-cfg-vol" {
+resource "docker_volume" "projectsendapp-cfg-vol" {
   name   = "vol-projectsendapp-cfg"
   driver = "local-persist"
   driver_opts = {
     "mountpoint" = var.projectsendapp_cfg_default_mountpoint
+  }
+}
+
+resource "docker_volume" "projectsendapp-dat-vol" {
+  name   = "vol-projectsendapp-dat"
+  driver = "local-persist"
+  driver_opts = {
+    "mountpoint" = var.projectsendapp_dat_default_mountpoint
   }
 }
 
